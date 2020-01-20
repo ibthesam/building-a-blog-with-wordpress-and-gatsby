5de1b60096dbd2638a5157f66497c439998adeba
karks88 <webmaster@karks.com>
2020-01-20 08:13:45 -0500

    Removed ACF References

--- src/templates/BlogPost.js
+++ src/templates/BlogPost.js
@@ -1,53 +1,27 @@
 import React from "react"
 import { graphql } from "gatsby"
-import Img from "gatsby-image"
-import Layout from "../components/layout"
+import Layout from "../components/layout.js"
 import SEO from "../components/seo"
 
-const BlogPostTemplate = ({ data }) => (
+const PageTemplate = ({ data }) => (
   <Layout>
     <SEO
-      title={data.wordpressPost.title}
-      description={data.wordpressPost.excerpt}
+      title={data.wordpressPage.title}
+      description={data.wordpressPage.excerpt}
     />
-    <h1>{data.wordpressPost.title}</h1>
-    <p>
-      Written by {data.wordpressPost.author.name} on {data.wordpressPost.date}
-    </p>
-    <Img
-      sizes={data.wordpressPost.acf.feat_img.localFile.childImageSharp.sizes}
-      alt={data.wordpressPost.title}
-      style={{ maxHeight: 450 }}
-    />
-    <div
-      style={{ marginTop: 20 }}
-      dangerouslySetInnerHTML={{ __html: data.wordpressPost.content }}
-    />
+    <h1>{data.wordpressPage.title}</h1>
+    <div dangerouslySetInnerHTML={{ __html: data.wordpressPage.content }} />
   </Layout>
 )
-export default BlogPostTemplate
+
+export default PageTemplate
 
 export const query = graphql`
   query($id: Int!) {
-    wordpressPost(wordpress_id: { eq: $id }) {
+    wordpressPage(wordpress_id: { eq: $id }) {
       title
-      content
       excerpt
-      date(formatString: "MMMM DD, YYYY")
-      author {
-        name
-      }
-      acf {
-        feat_img {
-          localFile {
-            childImageSharp {
-              sizes(maxWidth: 1200) {
-                ...GatsbyImageSharpSizes
-              }
-            }
-          }
-        }
-      }
+      content
     }
   }
 `
