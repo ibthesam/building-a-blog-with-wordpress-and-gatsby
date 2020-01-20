05d049cc1dc9976c1a195ae2f36173fc72f2f18e
karks88 <webmaster@karks.com>
2020-01-20 08:13:08 -0500

    Removed ACF References

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,69 +1,27 @@
 import React from "react"
-import { graphql, Link } from "gatsby"
-import Img from "gatsby-image"
-
-import Layout from "../components/layout"
+import { graphql } from "gatsby"
+import Layout from "../components/layout.js"
 import SEO from "../components/seo"
 
-const IndexPage = ({ data }) => (
+const PageTemplate = ({ data }) => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <ul style={{ listStyle: "none" }}>
-      {data.allWordpressPost.edges.map(post => (
-        <li style={{ padding: "20px 0", borderBottom: "1px solid #ccc" }}>
-          <Link
-            to={`/post/${post.node.slug}`}
-            style={{ display: "flex", color: "black", textDecoration: "none" }}
-          >
-            <Img
-              sizes={post.node.acf.feat_img.localFile.childImageSharp.sizes}
-              alt={post.node.title}
-              style={{ width: "25%", marginRight: 20 }}
-            />
-            <div style={{ width: "75%" }}>
-              <h3
-                dangerouslySetInnerHTML={{ __html: post.node.title }}
-                style={{ marginBottom: 0 }}
-              />
-              <p style={{ margin: 0, color: "grey" }}>
-                Written by {post.node.author.name} on {post.node.date}
-              </p>
-              <div dangerouslySetInnerHTML={{ __html: post.node.excerpt }} />
-            </div>
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <SEO
+      title={data.wordpressPage.title}
+      description={data.wordpressPage.excerpt}
+    />
+    <h1>{data.wordpressPage.title}</h1>
+    <div dangerouslySetInnerHTML={{ __html: data.wordpressPage.content }} />
   </Layout>
 )
 
-export default IndexPage
+export default PageTemplate
 
 export const query = graphql`
-  query {
-    allWordpressPost {
-      edges {
-        node {
-          title
-          excerpt
-          slug
-          author {
-            name
-          }
-          date(formatString: "MMMM DD, YYYY")
-          acf {
-            feat_img {
-              localFile {
-                childImageSharp {
-                  sizes(maxWidth: 600) {
-                    ...GatsbyImageSharpSizes
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
+  query($id: Int!) {
+    wordpressPage(wordpress_id: { eq: $id }) {
+      title
+      excerpt
+      content
     }
   }
 `
